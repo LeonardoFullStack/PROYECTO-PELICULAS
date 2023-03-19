31ccf4c2a56a0cf0f8477680b05c1dd89012e171
esteveleonardo@gmail.com <esteveleonardo@gmail.com>
2023-03-19 22:25:02 +0100

    primeros arreglos del search

--- controllers/frontControllers.js
+++ controllers/frontControllers.js
@@ -1,31 +1,38 @@
 const express = require('express')
-const {consulta} = require('../helpers/fetchImdb')
+const { consulta } = require('../helpers/fetchImdb')
 
 
-const getIndex =async (req, res,) => {
+const getIndex = async (req, res,) => {
 
 
 
-    res.render('index', {
-      titulo: 'Práctica node',
-      msg: 'Haz login para comenzar'
-    })
-}
-
-const getSearch =async (req, res) => {
-  const busqueda = req.query.query
-  console.log(busqueda)
-
-  const peticion =await consulta(busqueda)
-  console.log(peticion.results.length)
-  res.render('search', {
-    titulo: `Resultados de ${busqueda}`,
-    msg: `Se han encontrado ${peticion.results.length} resultados`,
-    data: peticion.results
+  res.render('index', {
+    titulo: 'Proyecto intermedio',
+    msg: 'Haz login para comenzar'
   })
 }
 
+const getSearch = async (req, res) => {
+  const busqueda = req.query.query
+  if (busqueda) {
+    const peticion = await consulta(busqueda)
+    console.log(peticion)
+    res.render('search', {
+      titulo: `Resultados de ${busqueda}`,
+      msg: `Se han encontrado ${peticion.results.length} resultados`,
+      query:true,
+      data: peticion.results
+    })
+  } else {
+    res.render('search', {
+      titulo: `Búsqueda de películas`,
+      msg: `Haz aquí tu búsqueda`,
+      query:false
+    })
+  }
+}
+
 module.exports = {
-    getIndex,
-    getSearch
+  getIndex,
+  getSearch
 }
